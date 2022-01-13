1807a823c4e9d3f4230230f29287c92e37f90c44
Nouvelle <93885095+Nouvelle-Zelande@users.noreply.github.com>
2022-01-13 01:39:49 +0000

    Added New Cheeses, Deleted cheeses with invalid suffixes (.jfif), made all files local, in the process removing cheese_storage. Bot now shows as typing for 3 seconds before sending generated cheese, as sometimes it is delayed, and i needed it typing to show it was working

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 # Imports
-import os
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 from keep_alive import keep_alive
-import random
-import asyncio
-import datetime as dt
-from cheese_storage import cheese
+import glob, random, asyncio, os
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 intents=discord.Intents.all()
 client=commands.Bot(command_prefix='ch!', intents=intents)
 prefix='ch!'
+file_path_type = "./cheeses/*.jpg"
+images = glob.glob(file_path_type)
+
 @client.event
 async def on_ready():
   print(f"{client.user} is connected to the following guilds:")
@@ -20,13 +19,15 @@ async def on_ready():
     print(f"Name: {guild.name}, ID: {guild.id}")
 @client.command(name="gen", help="Generates a random cheese")
 async def gen(ctx):
-  print(f"Gen command used by {ctx.author.display_name} in {ctx.author.guild.name}")
-  cheeselist=random.choice(cheese)
-  embed=discord.Embed(title="The cheese you have generated is:")
-  embed.set_image(url=cheeselist)
-  embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
-  embed.set_footer(text="Source: Misc Wikipedia Images")
-  await ctx.send("Generation Successful!", embed=embed)
+  async with ctx.typing():
+    print(f"Gen command used by {ctx.author.display_name} in {ctx.author.guild.name}")
+    embed=discord.Embed(title="The cheese you have generated is:")
+    file = discord.File(f"{random.choice(images)}", filename="image.png")
+    embed.set_image(url="attachment://image.png")
+    embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
+    embed.set_footer(text="Source: Misc Wikipedia and Google Images")
+    await asyncio.sleep(3)
+  await ctx.send("Generation Successful!", file=file, embed=embed)
 @client.command(name="ping", help="Pings bot to check whether it is active.")
 async def ping(ctx):
   print(f"Ping command used by {ctx.author.display_name} in {ctx.author.guild.name}")
